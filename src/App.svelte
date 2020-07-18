b0be908b7e48aa6368ad62c1fd724977c5f46cf2
Brad Garropy <bradgarropy@gmail.com>
2020-07-17 22:13:11 -0500

    added a router and two pages.

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,21 @@
-<h1>🤖 cybernetically enhanced svelte starter</h1>
+<script>
+    import {Router, Route, Link} from "svelte-routing"
+
+    import Home from "./components/Home.svelte"
+    import About from "./components/About.svelte"
+</script>
+
+<Router>
+    <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+    </nav>
+
+    <Route path="/">
+        <Home />
+    </Route>
+
+    <Route path="/about">
+        <About />
+    </Route>
+</Router>
---
+++ src/components/About.svelte
@@ -0,0 +1,13 @@
+<h1>🤖 cybernetically enhanced svelte starter</h1>
+
+<p>
+    Svelte is a radical new approach to building user interfaces. Whereas
+    traditional frameworks like React and Vue do the bulk of their work in the
+    browser, Svelte shifts that work into a compile step that happens when you
+    build your app.
+</p>
+
+<p>
+    Instead of using techniques like virtual DOM diffing, Svelte writes code
+    that surgically updates the DOM when the state of your app changes.
+</p>
